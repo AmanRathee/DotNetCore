75ab742c8c214332a2dbb05dc2c35077f6689e82
Aman Rathee <49388645+AmanRathee@users.noreply.github.com>
2019-04-11 12:11:08 +0530

    Initial commit

---
+++ EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>() {
+                                                    new Employee(){Id=1,Name="Neeti",Department="Teching",Email="gjhgjh@hghg.com" },
+                                                    new Employee(){Id=2,Name="Aman",Department="IT",Email="gjhgjh@hghg.com" },
+                                                    new Employee(){Id=3,Name="Kridha",Department="Fun",Email="gjhgjh@hghg.com" }
+                                                 };
+
+        }
+
+        public IEnumerable<Employee> GetAllEmployee()
+        {
+            return _employeeList;
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(x => x.Id == Id);
+        }
+    }
+}
---
+++ EmployeeManagement/Startup.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace EmployeeManagement
+{
+    public class Startup
+    {
+        private IConfiguration _config;
+
+        //Get the configuration keys from appsettings.json
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();//services.AddMvc() will just add the main MVC library,not the related ones
+            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>(); 
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,ILogger<Startup> logger)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            //*************************
+            //app.UseDefaultFiles();//this will run the default.html/default.htm/index.html/index.htm
+            //But if you want to make another html file as teh default html then use this overload:-
+            //DefaultFilesOptions dfo = new DefaultFilesOptions();
+            //dfo.DefaultFileNames.Clear();
+            //dfo.DefaultFileNames.Add("Home.html");
+            //app.UseDefaultFiles(dfo);
+            //*************************
+
+
+
+
+            app.UseStaticFiles();
+
+
+            app.UseMvcWithDefaultRoute();
+
+
+
+
+            //Middleware 1 :- app.Use will take an additional param "next" in the lambda expression 
+            //It also needs to add "await next()" so that the next middleware is called up.
+            app.Use(async (context,next) =>
+            {
+                logger.LogInformation("MW1 : Incoming");
+                await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+
+                //1.iisexpress when csproj has <AspNetCoreHostingModel>InProcess</AspNetCoreHostingModel>
+                //and Visual Studio F5 is used.By default ,VS uses IISEXPRESS
+
+                //2.dotnet when csproj has InProcess OR OutOfProcess 
+                //and CLI is used :- dotnet run.Here Kestrel is the server used.
+
+                //3.iisexpress when csproj has <AspNetCoreHostingModel>InProcess</AspNetCoreHostingModel>
+                //and Visual Studio F5 is used.By default ,VS uses IISEXPRESS
+
+                await next();
+                logger.LogInformation("MW1 : outGoing");
+            });
+
+             app.Use(async (context, next) =>
+            {
+                logger.LogInformation("MW2 : Incoming");
+                await context.Response.WriteAsync("test");
+                await next();
+                logger.LogInformation("MW2 : outGoing");
+            });
+
+            //Middleware 3
+
+            //Middleware 4 :- uses App.Run and acts like terminal middleware.
+            //The following middlewares after this one wont get executed.
+            app.Run(async (context) =>
+            {
+                //Read the appsetting config and write here
+                await context.Response.WriteAsync(_config["MyCustomKey"]);
+                logger.LogInformation("MW4 : Request Handled Completely.");
+
+            });
+        }
+    }
+}
